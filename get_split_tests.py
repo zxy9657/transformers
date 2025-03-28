11053ba063bfe504d2d2fb5117a7438764850cd2
ydshieh <ydshieh@users.noreply.github.com>
2025-03-28 17:52:18 +0100

    random!!!!!!!!!!

---
+++ get_split_tests.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # Required parameters
+    parser.add_argument(
+        "--target", default=None, type=str, required=True, help="Path to the output Tensorflow dump file."
+    )
+
+    args = parser.parse_args()
+
+    with open(args.target) as fp:
+        tests = fp.read().split('\n')
+
+    # split_idx = int(os.environ["CIRCLE_NODE_INDEX"])
+    # n_splits = int(os.environ["CIRCLE_NODE_TOTAL"])
+
+    split_idx = 0
+    n_splits = 8
+
+    n_tests_per_split = len(tests) // int(n_splits)
+    stat_idx, end_idx = n_tests_per_split * split_idx, n_tests_per_split * (split_idx + 1)
+    split_tests = tests[stat_idx:end_idx]
+
+    breakpoint()
+    split_tests = "\n".join(split_tests)
+
+    with open("splitted_tests.txt", "w") as fp:
+        fp.write(split_tests)
